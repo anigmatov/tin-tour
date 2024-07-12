4ef9a0e1bdbf5e5e649e2afacd4fe6b98f97cc57
Abdusamad Nigmatov <a.nigmatov@newuzbekistanuniversity.uz>
2024-07-12 23:32:11 +0500

    footer  ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,10 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
